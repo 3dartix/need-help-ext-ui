7c4c7d2c72aab378c98232c7b94b3f93bc05fe18
fz.pugachev <7197338-fz.pugachev@users.noreply.gitlab.com>
2022-07-13 12:20:38 +0200

    some refactoring

--- src/main/java/ru/pugart/ext/api/ui/controller/AuthController.java
+++ src/main/java/ru/pugart/ext/api/ui/controller/AuthController.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-//import reactor.core.publisher.Mono;
 import reactor.core.publisher.Mono;
 import ru.pugart.ext.api.ui.dto.JwtDto;
 import ru.pugart.ext.api.ui.dto.UserDto;
@@ -23,11 +22,10 @@ public class AuthController {
         return userDto
                 .log()
                 .flatMap(u -> {
-                    log.info("userDto: {}", u);
                     if(keycloakClient.createOrUpdateUser(u)) {
                         return Mono.just(keycloakClient.getTokenAndReset(u));
                     }
                     return Mono.empty();
-                }).switchIfEmpty(Mono.empty());
+                });
     }
 }
